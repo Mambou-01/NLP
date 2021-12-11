981fbeff69d5205ea0b842fa869201ea242d8baa
Mambou-01 <cmambou06@gmail.com>
2021-12-11 01:30:06 +0100

    update method bag_of_words in nltk_utils and re trained model

--- 02_chatBot/nltk_utils.py
+++ 02_chatBot/nltk_utils.py
@@ -33,7 +33,7 @@ def bag_of_words(tokenized_sentence,all_words):
    # stem each word
     sentence_words = [stem(word) for word in tokenized_sentence]
     # initialize bag with 0 for each word
-    bag = np.zeros(len(worall_wordsds), dtype=np.float32)
+    bag = np.zeros(len(all_words), dtype=np.float32)
     for idx, w in enumerate(all_words):
         if w in sentence_words: 
             bag[idx] = 1
